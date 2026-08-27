d6c522f17c4619621d61d7e97405c26030bb2a6d
sumsky19 <sumsky19@gmail.com>
2026-08-28 07:03:51 +0900

    Delete backend-config.js

--- backend-config.js
+++
@@ -1,5 +0,0 @@
-// Supabase 공개용 설정값입니다. Secret key/service_role key는 넣지 않습니다.
-window.BACKEND_CONFIG = {
-  url: "https://olbeyljtvefjdrpyhtjq.supabase.co",
-  anonKey: "sb_publishable_tyM9IJHggQnbr5b4uj9tXw_YpqZ_R7w"
-};
